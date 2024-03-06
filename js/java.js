c87bdc3d6ccf98e64dceac200826002c5792e73b
Eliel <125eramirez@frhsd.com>
2024-03-06 09:48:57 -0500

    NEW CHANGES

--- js/java.js
+++ js/java.js
@@ -1,4 +1,22 @@
-// Initialization for ES Users
-import { Dropdown, initMDB } from "mdb-ui-kit";
+const buttons = document.querySelectorAll(".radio-wrapper");
 
-initMDB({ Dropdown });
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        updateContent(button.id);
+    })
+});
+
+function updateContent(date) {
+console.log(date);
+    switch (date) {
+        case "1894":
+            // Change Text
+            // Change Picture
+            console.log("it worked");
+            break;
+            
+        default:
+            alert("something went wrong");
+    }
+
+}
